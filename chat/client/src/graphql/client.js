890458eeeb82aefddbe13dfc0c27f703a8980740
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-12-06 16:37:02 -0600

    Apollo client configuration ready. GraphQL-ws client

--- chat/client/src/graphql/client.js
+++ chat/client/src/graphql/client.js
@@ -1,9 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { Kind, OperationTypeNode } from 'graphql';
+import { createClient as createWsClient } from 'graphql-ws';
 
-const GRAPHQL_URL = 'http://localhost:9000/graphql';
+const httpLink = new HttpLink({
+  uri: 'http://localhost:9000/graphql',
+});
+
+const wsLink = new GraphQLWsLink(createWsClient({
+  url: 'ws://localhost:9000/graphql'
+}));
+
+function isSubscription ({ query }) {
+  const definition = getMainDefinition(query);
+  return definition.kind === Kind.OPERATION_DEFINITION && definition.operation === OperationTypeNode.SUBSCRIPTION;
+}
 
 export const client = new ApolloClient({
-  uri: GRAPHQL_URL,
+  link: split(),
   cache: new InMemoryCache(),
 });
 
